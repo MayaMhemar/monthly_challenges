c73e13c84aee8f2a0fc301ef6eb4e60159da1ebe
MayaMhemar <mayamkhemar13@gmail.com>
2023-08-25 18:51:10 +0200

    .

--- challenges/views.py
+++ challenges/views.py
@@ -5,20 +5,28 @@ monthly_challenges = {
     "january": "Eat no meat for the entire month!",
     "fabruary" : "Walk for at least 20 minutes every day!",
     "march" : "Learn Django for at least 20 minutes every day!",
-    "april" :"Eat no meat for the entire month!"
+    "april" : "Walk for at least 20 minutes every day!",
+    "may" :"Eat no meat for the entire month!",
+    "june" : "Walk for at least 20 minutes every day!",
+    "july" : "Learn Django for at least 20 minutes every day!",
+    "august" :"Eat no meat for the entire month!",
+    "september" : "Walk for at least 20 minutes every day!",
+    "october" : "Learn Django for at least 20 minutes every day!",
+    "november" :"Eat no meat for the entire month!",
+    "december" : "Walk for at least 20 minutes every day!"
 }
+
 # Create your views here.
+
 
 def monthly_challenge_by_number(request, month):
     return HttpResponse(month)
 
+
 def monthly_challenge(request, month):
-    if month == "january":
-        challenge_text = "Eat no meat for the entire month!"
-    elif month == "fabruary":
-        challenge_text = "Walk for at least 20 minutes every day!"
-    elif month == "march":
-        challenge_text ="Learn Django for at least 20 minutes every day!"
-    else:
-        return HttpResponseNotFound("This month is not supported!")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")    
+    
